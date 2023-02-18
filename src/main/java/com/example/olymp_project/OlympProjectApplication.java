9b662403fddb3e8caf5ca9829a8e8a7cae7bbe32
Vadim-Kulkov <manavar2017@yandex.com>
2023-02-18 19:00:53 +0400

    Initial commit

---
+++ src/main/java/com/example/olymp_project/OlympProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.olymp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympProjectApplication.class, args);
+    }
+
+}
